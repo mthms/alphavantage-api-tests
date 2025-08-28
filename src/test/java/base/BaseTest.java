7bedc0f92828465577ae32410c0510b65a7527e7
mthms <mtharwat551@outlook.com>
2025-08-28 23:33:15 +0300

    Introducing logs xml controller and initials vars into the MDC to pass over to logger

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,24 @@
 package base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+
 public class BaseTest {
+
+    private static final ThreadLocal<Logger> logger = new ThreadLocal<>();
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup(ITestResult result, Method method) {
+        Reporter.log("Setup process for test: " + method.getName() + " has started...", true);
+        MDC.put("testCase", method.getName());
+        MDC.put("threadId", String.valueOf(Thread.currentThread().threadId()));
+        logger.set(LoggerFactory.getLogger("BaseTest.class"));
+
+    }
 }
