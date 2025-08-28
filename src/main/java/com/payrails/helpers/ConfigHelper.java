ae98ec832078737de80e3e0f9044dfb9683ea07d
mthms <mtharwat551@outlook.com>
2025-08-29 00:48:54 +0300

    initial logic for readConfigs method

--- src/main/java/com/payrails/helpers/ConfigHelper.java
+++ src/main/java/com/payrails/helpers/ConfigHelper.java
@@ -1,4 +1,15 @@
 package com.payrails.helpers;
 
-public class DataHelper {
+import com.payrails.models.Configs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigHelper {
+    private static final Logger logger = LoggerFactory.getLogger(ConfigHelper.class);
+
+    public Configs readConfigs(Configs configs){
+        logger.info("Initiating the main configs reading process...");
+
+        return configs;
+    }
 }
