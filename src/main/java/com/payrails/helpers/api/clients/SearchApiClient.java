5e1ecbbbcd87bfc510dcee5785b81643bd5f9c14
mthms <mtharwat551@outlook.com>
2025-08-31 17:55:32 +0300

    Adding the searchApiRequest method

--- src/main/java/com/payrails/helpers/api/clients/SearchApiClient.java
+++ src/main/java/com/payrails/helpers/api/clients/SearchApiClient.java
@@ -1,4 +1,38 @@
 package com.payrails.helpers.api.clients;
 
+import com.payrails.models.Configs;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SearchApiClient {
+    private static final Logger logger = LoggerFactory.getLogger(SearchApiClient.class);
+    private final Configs configs;
+
+    public SearchApiClient(Configs configs) {
+        this.configs = configs;
+    }
+
+    public Response searchSymbols(List<String> keywords) {
+        // Merge keywords into a single comma-separated string
+        String mergedKeywords = keywords.stream()
+                .map(String::trim)
+                .collect(Collectors.joining(","));
+
+        // Build and send the request
+        return RestAssured
+                .given()
+                .queryParam("function", "SYMBOL_SEARCH")
+                .queryParam("keywords", mergedKeywords)
+                .queryParam("apikey", configs.getAlphaVantageApiKey())
+                .when()
+                .get(configs.getAlphaVantageBaseUrl())
+                .then()
+                .extract()
+                .response();
+    }
 }
