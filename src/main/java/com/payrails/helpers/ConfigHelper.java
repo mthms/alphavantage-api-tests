ac1403ec4bad5f5954e886251ca169d1cd0d3df9
mthms <mtharwat551@outlook.com>
2025-08-31 17:43:08 +0300

    Adding the keys readers for the baseURL and the apiKey

--- src/main/java/com/payrails/helpers/ConfigHelper.java
+++ src/main/java/com/payrails/helpers/ConfigHelper.java
@@ -4,6 +4,8 @@ import com.payrails.models.Configs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.Properties;
 
 public class ConfigHelper {
@@ -11,6 +13,21 @@ public class ConfigHelper {
 
     public Configs readConfigs(Configs configs){
         logger.info("Initiating the main configs reading process...");
+
+        File mainConfigs = new File("resources/environments/config_"
+                + System.getProperty("env", "testing").toLowerCase()
+                + ".properties");
+
+        try {
+            logger.info("Reading main configs...");
+            FileReader fileReader = new FileReader(mainConfigs);
+            Properties properties = new Properties();
+            properties.load(fileReader);
+            configs.setAlphaVantageApiKey(getPropertyValueOrDefault(properties, "alphaVantageApiKey", ""));
+            configs.setAlphaVantageBaseUrl(getPropertyValueOrDefault(properties, "alphaVantageBaseUrl", ""));
+        } catch (Exception e){
+            logger.error("Reading main config file failed. Killing the test...", e);
+        }
 
         logger.info("Finalized reading the main configs successfully.");
         return configs;
--- src/main/java/com/payrails/models/Configs.java
+++ src/main/java/com/payrails/models/Configs.java
@@ -1,4 +1,22 @@
 package com.payrails.models;
 
 public class Configs {
+    private String alphaVantageApiKey;
+    private String alphaVantageBaseUrl;
+
+    public String getAlphaVantageApiKey() {
+        return alphaVantageApiKey;
+    }
+
+    public void setAlphaVantageApiKey(String alphaVantageApiKey) {
+        this.alphaVantageApiKey = alphaVantageApiKey;
+    }
+
+    public String getAlphaVantageBaseUrl() {
+        return alphaVantageBaseUrl;
+    }
+
+    public void setAlphaVantageBaseUrl(String alphaVantageBaseUrl) {
+        this.alphaVantageBaseUrl = alphaVantageBaseUrl;
+    }
 }
