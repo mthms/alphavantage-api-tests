ad72394f329f85660ae907efdfb0c670bc0cfd28
mthms <mtharwat551@outlook.com>
2025-08-31 16:02:12 +0300

    Add a method to overrride default behaviour when a key exists in configs but it's value is empty to avoid setting it to null

--- src/main/java/com/payrails/helpers/ConfigHelper.java
+++ src/main/java/com/payrails/helpers/ConfigHelper.java
@@ -4,6 +4,8 @@ import com.payrails.models.Configs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Properties;
+
 public class ConfigHelper {
     private static final Logger logger = LoggerFactory.getLogger(ConfigHelper.class);
 
@@ -12,5 +14,12 @@ public class ConfigHelper {
 
         logger.info("Finalized reading the main configs successfully.");
         return configs;
+    }
+
+    public String getPropertyValueOrDefault(Properties properties, String keyName, String defaultValue){
+        if (properties.getProperty( keyName, defaultValue).isEmpty())
+            return defaultValue;
+        else
+            return properties.getProperty( keyName, defaultValue);
     }
 }
